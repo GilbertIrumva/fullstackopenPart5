9e28aee0722c641a88c39ee40379cb4af0ca1c71
Gilbertirumva <gilbertirumva25@gmail.com>
2026-06-29 21:45:27 +0300

    Initial commit: bloglist backend (part 4) + frontend exercise 5.1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    if (user) {
+      blogService.getAll().then((blogs) => setBlogs(blogs))
+    }
+  }, [user])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedUser = await loginService.login({ username, password })
+      setUser(loggedUser)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong username or password')
+      setTimeout(() => setErrorMessage(null), 5000)
+    }
+  }
+
+  const errorNotification = () =>
+    errorMessage && (
+      <div style={{ color: 'red', border: '1px solid red', padding: 8 }}>
+        {errorMessage}
+      </div>
+    )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {errorNotification()}
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {errorNotification()}
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ server/controllers/blogs.js
@@ -0,0 +1,66 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const user = request.user
+
+  if (!user) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: likes || 0,
+    user: user._id,
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(204).end()
+  }
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(403).json({ error: 'only the creator can delete this blog' })
+  }
+
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const updated = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    { new: true, runValidators: true, context: 'query' }
+  )
+
+  if (!updated) {
+    return response.status(404).end()
+  }
+  response.json(updated)
+})
+
+module.exports = blogsRouter
